89479b6866b4013ea0968bd888c0dbc94405be6d
Rían Errity <rian@paradaux.io>
2022-11-21 05:21:08 +0100

    Footer

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.css"
+
+export default function Footer() { 
+    return (
+        <div className="footer">
+            &copy; Rían Errity, source made available under AGPL<a href="">here</a>.
+        </div>
+    )
+}
