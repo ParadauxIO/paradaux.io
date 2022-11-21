9abd68003f9fa17704dda3d7cf2d74fc143560dd
Rían Errity <rian@paradaux.io>
2022-11-21 05:32:15 +0100

    Add link to repo

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css"
 export default function Footer() { 
     return (
         <div className="footer">
-            &copy; Rían Errity, source made available under AGPL<a href="">here</a>.
+            &copy; Rían Errity, source made available under AGPL<a href="https://github.com/ParadauxIO/paradaux.io">here</a>.
         </div>
     )
 }
