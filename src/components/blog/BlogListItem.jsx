de8cfce257ba57af69bff34edf0573cafb38cd6a
Rían Errity <rian@paradaux.io>
2022-11-21 05:21:55 +0100

    Index blog summary component

---
+++ src/components/blog/BlogListItem.jsx
@@ -0,0 +1,44 @@
+import "./BlogListItem.css"
+
+export default function BlogListItem({ }) {
+    let day = 1;
+    let month = "May";
+    let header = "This is an insightful heading."
+
+    return (
+        <div className="blog-list-item">
+            <div className="blog-list-left">
+                <div className="blog-list-date">
+                    <span className="day">
+                        {day}<sup className="day-suffix">{getDateDaySuffix(day)}</sup>
+                    </span>
+                    <span className="month">{month}</span>
+                </div>
+                <div className="text-sideways">
+                    category
+                </div>
+            </div>
+            <div className="blog-list-right">
+                <h1>{ header }</h1>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis neque quas tenetur dicta ut. Excepturi quasi sed tenetur quod odit,
+                    ratione fuga suscipit nihil, magni quaerat, voluptate facere quae labore.
+
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est voluptate inventore omnis provident doloremque aperiam quia illo tempore
+                    non ad. Cum, numquam explicabo quae hic aut placeat animi consequatur beatae? <a href="">Read more.</a>
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+function getDateDaySuffix(day) {
+    switch (day) { 
+        case 1:
+            return "st";
+        case 2:
+            return "nd";
+        
+    }
+ }
