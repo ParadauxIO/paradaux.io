5361973cfa16c71dafe7540665912c28253708c0
Rían Errity <rian@paradaux.io>
2023-03-30 04:20:57 +0200

    Fix up navbar for desktop and mobile

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -18,11 +18,11 @@ export default function Navbar({hasBg}) {
         <>
             <header className={hasBg ? "is-scrolling" : ""}>
                 <div className="container">
-                    <h2><span>Rían</span> Errity</h2>
+                    <h2 className="mb-header"><span>Rían</span> Errity</h2>
                     <nav className="desktop-nav">
                         <a href="https://www.linkedin.com/in/r%C3%ADan-errity/">LinkedIn</a>
                         <a href="https://github.com/ParadauxIO">Github</a>
-                        <a href="https://paradaux.io/static/cv.pdf">C.V / Résumé</a>
+                        <a href="/cv.pdf">C.V / Résumé</a>
                         <a href="https://clove.ie">Clove Software</a>
                         <a href="https://discord.gg/62q2GKXCk4">Discord</a>
                         <a href="https://instagram.com/rian.errity">Instagram</a>
@@ -40,7 +40,7 @@ export default function Navbar({hasBg}) {
                     src="../../images/rian-headshot.png"
                 />
                 <p>
-                    Hi there! I'm Rían &mdash; I'm a {calculate_age(new Date(2002, 5, 13))}-year-old developer from Ireland ! Feel free to reach out, I'd love to meet you. When I'm not writing code, or sleeping I'm studying Computer Science & Linguistics at Trinity College, Dublin.
+                    <span>Hi there &mdash;</span> I'm a {calculate_age(new Date(2002, 5, 13))}-year-old Software Engineer from Ireland. I'm currently in the process of finishing up my degree in Computer Science and Linguistics at Trinity College Dublin and working on my startup <a href="https://clove.ie" className="bio-link">clove.ie</a>
                 </p>
 
                 <ul>
@@ -48,7 +48,7 @@ export default function Navbar({hasBg}) {
                         <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                             <path fill="#d1d5d6" d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"/>
                         </svg>
-                        <a href="">rian@paradaux.io</a>
+                        <a href="mailto:rian@paradaux.io">rian@paradaux.io</a>
                     </li>
                 </ul>
                 <div className="links">
