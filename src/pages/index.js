4b7513d92e5f12232640dd23b9c0416679e5872c
Rían Errity <rian@paradaux.io>
2023-03-30 04:31:37 +0200

    More or less finished index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,43 @@ import BlogListItem from "../components/blog/BlogListItem"
 import Navbar from "../components/common/Navbar"
 import "./index.scss"
 import { graphql } from 'gatsby'
+import Footer from "../components/common/Footer"
+import { StaticImage } from "gatsby-plugin-image"
+
+function calculate_age(dob) { 
+  var diff_ms = Date.now() - dob.getTime();
+  var age_dt = new Date(diff_ms); 
+
+  return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
 
 const IndexPage = ({ data }) => {
   return (
       <main>
         <Navbar hasBg={true}/>
-        <section className="blog">
-          <h1 className="section-title">Blog</h1>
-          {data.allMdx.nodes.map(item => <BlogListItem key={item.id} item={item}/>)}
-        </section>
+        <div className="content">
+            <section className="bio">
+              <div className="bio-content">
+                <h1>Rían <span>Errity</span></h1>
+                <div className="bio-card">
+                  
+                  <StaticImage
+                    src="../images/rian-headshot.png"
+                    alt="A picture of Rían in Paris"
+                  />
+                  <p>
+                    <span>Hi there &mdash;</span> I'm a {calculate_age(new Date(2002, 5, 13))}-year-old Software Engineer from Ireland. I'm currently in the process of finishing up my degree in Computer Science and Linguistics at Trinity College Dublin and working on my startup <a href="https://clove.ie">clove.ie</a>
+                  </p>
+
+                  <p>Here you can find various essays of mine, as well as tutorials and code snippets you might find useful.</p>
+                </div>
+              </div>
+            </section>
+            <section className="blog">
+              <h1 className="section-title">Blog</h1>
+              {data.allMdx.nodes.map(item => <BlogListItem key={item.id} item={item}/>)}
+            </section>
+        </div>
       </main>
   )
 }
