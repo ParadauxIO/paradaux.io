ed514091e4cb7da1e6ffbfbde9d32bbdc84d736f
Rían Errity <rian@paradaux.io>
2023-07-30 00:52:23 +0100

    Remove homegrown analytics for now

--- src/analytics/AnalyticsComponent.jsx
+++
@@ -1,6 +0,0 @@
-import useAnalytics from "./useAnalytics"
-
-export default function AnalyticsComponent({page}) {
-    let analytics = useAnalytics(page);
-    return <></>
-}
--- src/analytics/store/sessionAtom.js
+++
@@ -1,3 +0,0 @@
-import { persistentAtom } from '@nanostores/persistent';
-
-export const sessionIdAtom = persistentAtom('session_id', '{"id": "00000000-0000-0000-0000-000000000000"}')
